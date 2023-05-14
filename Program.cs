f19bda27542c1058696b19bd98eb8ae0ad76443f
thisfozz <kfcfozz@vk.com>
2023-05-14 13:24:46 +0500

    Убрал лишнее

--- Program.cs
+++ Program.cs
@@ -12,11 +12,6 @@ namespace StudyCSharp
     {
         static void Main(string[] args)
         {
-            Money price = new Money(10, 50);
-            Product product = new Product("Молоко", price);
-            product.Price.Print(); // 10.50
-            product.ReducePrice(2, 30);
-            product.Price.Print(); // 8.20
         }
     }
 }
